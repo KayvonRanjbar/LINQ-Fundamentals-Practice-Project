f794e76fba8eba7ef6db8482bbbb20b827306954
Kayvon Ranjbar <kayvon.ranjbar@yahoo.com>
2020-09-27 10:02:25 -0600

    Practice using Func and Action Types

--- FeaturesPractice/Program.cs
+++ FeaturesPractice/Program.cs
@@ -12,8 +12,27 @@ namespace FeaturesPractice
         // Create some data that are of types that implement IEnumerable<T>
         // Create an extension method extending IEnumerable<T>
         // Use a Named Method, Anonymous Method, and finally a Lambda Expression to define filter criteria
+        // Create a Func<> that cubes an integer; a Func<> that subtracts 2 integers; update the filtering criteria; add ordering
         static void Main(string[] args)
         {
+            Func<int, int> cube = y => y * y * y;
+            // Without a full body block
+            // Func<int, int, int> subtract = (a, b) => a - b;
+
+            // With a full body block
+            Func<int, int, int> subtract = (a, b) =>
+            {
+                int result = a - b;
+                return result;
+            };
+
+            Action<int> write = x => Console.WriteLine(x);
+
+            // Without action
+            //Console.WriteLine(cube(subtract(5, 3)));
+            // With action
+            write(cube(subtract(5, 3)));
+
             IEnumerable<Customer> brickAndMortarCustomers = new Customer[]
             {
                 new Customer { Id = 1, FullName = "Joe Smith" },
@@ -26,16 +45,16 @@ namespace FeaturesPractice
                 new Customer { Id = 4, FullName = "Bob Plank"}
             };
 
-            /*
             // Named Method
+            /*
             foreach (var customer in brickAndMortarCustomers.Where(FullNameStartsWithM))
             {
                 Console.WriteLine(customer.FullName);
             }
             */
 
-            /*
             // Anonymous Method
+            /*
             foreach (var customer in brickAndMortarCustomers.Where(
                 delegate (Customer customer)
                 {
@@ -47,7 +66,12 @@ namespace FeaturesPractice
             */
 
             // Lambda Expression
-            foreach (var customer in brickAndMortarCustomers.Where(c => c.FullName.StartsWith("M")))
+            /*
+             * foreach (var customer in brickAndMortarCustomers.Where(c => c.FullName.StartsWith("M")))
+             */
+            // Change filtering and order
+            foreach (var customer in brickAndMortarCustomers.Where(c => c.FullName.Contains("a"))
+                                                            .OrderByDescending(c => c.FullName))
             {
                 Console.WriteLine(customer.FullName);
             }
